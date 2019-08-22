12826796178e100728204ec7fd3bf614dca00492
dherkyan <dherkyan@gmail.com>
2019-08-22 04:48:25 -0400

    Add serviços MVC e roteamento padrão

--- TDS.Web/Startup.cs
+++ TDS.Web/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TDS.Web
@@ -9,19 +8,13 @@ namespace TDS.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
